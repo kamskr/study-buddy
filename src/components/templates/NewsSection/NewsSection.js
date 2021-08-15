15113781db6e7e59b579df6efec1e9dd4ace0b7c
Kamil <sikorakamil26@gmail.com>
2021-08-15 21:11:11 +0200

    feat: connected to DatoCMS

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -1,61 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ArticleWrapper, ContentWrapper, NewsSectionHeader, TitleWrapper, Wrapper } from './NewsSection.styled';
 import { Button } from 'components/atoms/Button/Button';
+import axios from 'axios';
 
-const data = [
-  {
-    title: 'New computers at school',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet pretium nisi. In dictum accumsan ante, eget sodales diam laoreet vel. Donec cursus purus sit amet arcu pretium, eu elementum nulla sollicitudin. Morbi sed elit in est cursus blandit quis ac justo. Maecenas non magna maximus, dignissim ligula ut, laoreet sem. Cras ac conse',
-  },
-  {
-    title: 'New computers at school2',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet pretium nisi. In dictum accumsan ante, eget sodales diam laoreet vel. Donec cursus purus sit amet arcu pretium, eu elementum nulla sollicitudin. Morbi sed elit in est cursus blandit quis ac justo. Maecenas non magna maximus, dignissim ligula ut, laoreet sem. Cras ac conse',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: 'New computers at school3',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet pretium nisi. In dictum accumsan ante, eget sodales diam laoreet vel. Donec cursus purus sit amet arcu pretium, eu elementum nulla sollicitudin. Morbi sed elit in est cursus blandit quis ac justo. Maecenas non magna maximus, dignissim ligula ut, laoreet sem. Cras ac conse',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: 'New computers at school4',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet pretium nisi. In dictum accumsan ante, eget sodales diam laoreet vel. Donec cursus purus sit amet arcu pretium, eu elementum nulla sollicitudin. Morbi sed elit in est cursus blandit quis ac justo. Maecenas non magna maximus, dignissim ligula ut, laoreet sem. Cras ac conse',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: 'New computers at school5',
-    category: 'Tech news',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet pretium nisi. In dictum accumsan ante, eget sodales diam laoreet vel. Donec cursus purus sit amet arcu pretium, eu elementum nulla sollicitudin. Morbi sed elit in est cursus blandit quis ac justo. Maecenas non magna maximus, dignissim ligula ut, laoreet sem. Cras ac conse',
-    image: 'https://unsplash.it/500/400',
-  },
-];
+const query = `
+{
+  allArticles {
+    id
+    title
+    category
+    content
+    image {
+      url
+    }
+  }
+}
+`;
 
 export const NewsSection = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_DATOCMS_ENDPOINT);
+    axios
+      .post(
+        process.env.REACT_APP_DATOCMS_ENDPOINT,
+        {
+          query: query,
+        },
+        { headers: { authorization: `Bearer ${process.env.REACT_APP_DATOCMS_TOKEN}` } },
+      )
+      .then(({ data: { data } }) => {
+        setArticles(data.allArticles);
+      })
+      .catch(() => setError("Sorry, we couldn't load articles for you"));
+  }, []);
+
   return (
     <Wrapper>
       <NewsSectionHeader>News feed section</NewsSectionHeader>
-      {data.map(({ title, category, content, image = null }) => (
-        <ArticleWrapper key={title}>
-          <TitleWrapper>
-            <h3>{title}</h3>
-            <p>{category}</p>
-          </TitleWrapper>
-          <ContentWrapper>
-            <p>{content}</p>
-            {image ? <img src={image} alt="article" /> : null}
-          </ContentWrapper>
-          <Button isBig>click me</Button>
-        </ArticleWrapper>
-      ))}
+      {articles.length > 0 ? (
+        articles.map(({ title, category, content, image = null }) => (
+          <ArticleWrapper key={title}>
+            <TitleWrapper>
+              <h3>{title}</h3>
+              <p>{category}</p>
+            </TitleWrapper>
+            <ContentWrapper>
+              <p>{content}</p>
+              {image ? <img src={image.url} alt="article" /> : null}
+            </ContentWrapper>
+            <Button isBig>click me</Button>
+          </ArticleWrapper>
+        ))
+      ) : (
+        <NewsSectionHeader>{error ? error : 'Loading...'}</NewsSectionHeader>
+      )}
     </Wrapper>
   );
 };
